ab0977438a39aeb801fca004afd223aff22fded7
Varun Nagpal <95134445+SpyzzVVarun@users.noreply.github.com>
2024-01-12 23:24:18 +0530

    customized survey form

--- client/src/json.js
+++ client/src/json.js
@@ -1,203 +1,208 @@
+let productName = "SampleProduct";
+let disappointing = "How did we disappoint you and what can we do to make things right?"
+let improvements = "What can we do to make your experience more satisfying?";
+let checkboxes = ["Performance"
+, "User Interface"
+, "Quality Support"
+, "Stability"
+, "Documentations and Demos"];
+let entities = []
+for (let checkbox of checkboxes){
+    entities.push({"value": checkbox, "text": checkbox});
+}
 export const json = {
-    "completedHtmlOnCondition": [
-     {
-      "expression": "{nps-score} <= 6 or {rebuy} = false",
-      "html": {
-       "default": "Thanks for your feedback! We highly value all ideas and suggestions from our customers, whether they're positive or critical. In the future, our team might reach out to you to learn more about how we can further improve our product so that it exceeds your expectations.",
-       "fr": "Merci pour vos commentaires! Nous accordons une grande importance à toutes les idées et suggestions de nos clients, qu'elles soient positives ou critiques. À l'avenir, notre équipe pourrait vous contacter pour en savoir plus sur la façon dont nous pouvons encore améliorer notre produit afin qu'il dépasse vos attentes."
-      }
-     },
-     {
-      "expression": "{nps-score} = 6 or {nps-score} = 7",
-      "html": {
-       "default": "Thanks for your feedback. Our goal is to create the best possible product, and your thoughts, ideas, and suggestions play a major role in helping us identify opportunities to improve.",
-       "fr": "Merci pour vos commentaires. Notre objectif est de créer le meilleur produit possible, et vos réflexions, idées et suggestions jouent un rôle majeur pour nous aider à identifier les opportunités d'amélioration."
-      }
-     },
-     {
-      "expression": "{nps-score} >= 8",
-      "html": {
-       "default": "Thanks for your feedback. It's great to hear that you're a fan of our product. Your feedback helps us discover new opportunities to improve it and make sure you have the best possible experience.",
-       "fr": "Merci pour vos commentaires. Nous sommes ravis d'entendre que vous avez apprécié notre produit. Vos commentaires nous aident à découvrir de nouvelles opportunités pour l'améliorer et vous assurer la meilleure expérience possible."
-      }
-     }
-    ],
-    "pages": [
-      {
-        "name": "page1",
-        "elements": [
-          {
-            "type": "rating",
-            "name": "nps-score",
-            "title": {
-              "default": "On a scale from 0 to 10, how likely are you to recommend us to a friend or colleague?",
-              "fr": "Sur une échelle de 0 à 10, quelle est la probabilité que vous recommandiez notre produit à un ami ou à un collègue?"
-            },
-            "rateMin": 0,
-            "rateMax": 10,
-            "minRateDescription": {
-              "default": "Very unlikely",
-              "fr": "Très improbable"
-            },
-            "maxRateDescription": {
-              "default": "Very likely",
-              "fr": "Très probable"
-            }
+  "completedHtmlOnCondition": [
+   {
+    "expression": "{nps-score} <= 7 or {rebuy} = false",
+    "html": {
+     "default": "Thanks for your feedback! We highly value all ideas and suggestions from our customers, whether they're positive or critical. In the future, our team might reach out to you to learn more about how we can further improve our product so that it exceeds your expectations.",
+     "fr": "Merci pour vos commentaires! Nous accordons une grande importance à toutes les idées et suggestions de nos clients, qu'elles soient positives ou critiques. À l'avenir, notre équipe pourrait vous contacter pour en savoir plus sur la façon dont nous pouvons encore améliorer notre produit afin qu'il dépasse vos attentes."
+    }
+   },
+   {
+    "expression": "{nps-score} = 7 or {nps-score} = 8",
+    "html": {
+     "default": "Thanks for your feedback. Our goal is to create the best possible product, and your thoughts, ideas, and suggestions play a major role in helping us identify opportunities to improve.",
+     "fr": "Merci pour vos commentaires. Notre objectif est de créer le meilleur produit possible, et vos réflexions, idées et suggestions jouent un rôle majeur pour nous aider à identifier les opportunités d'amélioration."
+    }
+   },
+   {
+    "expression": "{nps-score} >= 8",
+    "html": {
+     "default": "Thanks for your feedback. It's great to hear that you're a fan of our product. Your feedback helps us discover new opportunities to improve it and make sure you have the best possible experience.",
+     "fr": "Merci pour vos commentaires. Nous sommes ravis d'entendre que vous avez apprécié notre produit. Vos commentaires nous aident à découvrir de nouvelles opportunités pour l'améliorer et vous assurer la meilleure expérience possible."
+    }
+   }
+  ],
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "rating",
+          "name": "nps-score",
+          "title": {
+            "default": `On a scale from 0 to 10, how likely are you to recommend ${productName} to a friend or colleague?`,
+            "fr": "Sur une échelle de 0 à 10, quelle est la probabilité que vous recommandiez notre produit à un ami ou à un collègue?"
           },
-          {
-            "type": "comment",
-            "name": "disappointing-experience",
-            "visibleIf": "{nps-score} <= 5",
-            "title": {
-              "default": "How did we disappoint you and what can we do to make things right?",
-              "fr": "Nous n'avons pas été a la hauteur de vos attentes, comment pouvons-nous améliorer?"
-            },
-            "maxLength": 300
+          "rateMin": 0,
+          "rateMax": 10,
+          "minRateDescription": {
+            "default": "Very unlikely",
+            "fr": "Très improbable"
           },
-          {
-            "type": "comment",
-            "name": "improvements-required",
-            "visibleIf": "{nps-score} >= 6",
-            "title": {
-              "default": "What can we do to make your experience more satisfying?",
-              "fr": "Que pouvons-nous faire pour rendre votre expérience plus satisfaisante?"
-            },
-            "maxLength": 300
-          },
-          {
-            "type": "checkbox",
-            "name": "promoter-features",
-            "visibleIf": "{nps-score} >= 9",
-            "title": {
-              "default": "Which of the following features do you value the most?",
-              "fr": "Laquelle des fonctionnalités suivantes appréciez-vous le plus ?"
-            },
-            "description": {
-              "default": "Please select no more than three features.",
-              "fr": "Veuillez ne pas sélectionner plus de trois fonctionnalités."
-            },
-            "isRequired": true,
-            "choices": [
-              {
-                "value": "performance",
-                "text": "Performance"
-              },
-              {
-                "value": "stability",
-                "text": {
-                  "default": "Stability",
-                  "fr": "Stabilité"
-                }
-              },
-              {
-                "value": "ui",
-                "text": {
-                  "default": "User interface",
-                  "fr": "Interface utilisateur"
-                }
-              },
-              {
-                "value": "complete-functionality",
-                "text": {
-                  "default": "Complete functionality",
-                  "fr": "Ensemble des fonctionnalités"
-                }
-              },
-              {
-                "value": "learning-materials",
-                "text": {
-                  "default": "Learning materials (documentation, demos, code examples)",
-                  "fr": "Matériel d'apprentissage (documentation, démos, exemples de code)"
-                }
-              },
-              {
-                "value": "support",
-                "text": {
-                  "default": "Quality support",
-                  "fr": "Accompagnement de qualité"
-                }
-              }
-            ],
-            "showOtherItem": true,
-            "otherPlaceholder": {
-              "default": "Please specify...",
-              "fr": "Veuillez préciser..."
-            },
-            "otherText": {
-              "default": "Other features",
-              "fr": "Autres fonctionnalités"
-            },
-            "colCount": 2,
-            "maxSelectedChoices": 3
+          "maxRateDescription": {
+            "default": "Very likely",
+            "fr": "Très probable"
           }
-        ]
-      },
-      {
-        "name": "page2",
-        "elements": [
-          {
-            "type": "boolean",
-            "name": "rebuy",
-            "title": {
-              "default": "Would you buy our product again?",
-              "fr": "Achèteriez-vous à nouveau notre produit?"
-            }
+        },
+        {
+          "type": "comment",
+          "name": "disappointing-experience",
+          "visibleIf": "{nps-score} <= 6",
+          "title": {
+            "default": disappointing,
+            "fr": "Nous n'avons pas été a la hauteur de vos attentes, comment pouvons-nous améliorer?"
           },
-        ]
-      },
-      {
-        "name": "page3",
-        "elements": [
-          {
-            "type": "radiogroup",
-            "name": "testimonial",
-            "title": {
-              "default": "Would you mind providing us a brief testimonial for the website?",
-              "fr": "Accepteriez-vous de rédiger un bref commentaire pour notre site Internet?"
-            },
-            "choices": [
-              {
-                "value": "yes",
-                "text": {
-                  "default": "Sure!",
-                  "fr": "Bien sur!"
-                }
-              },
-              {
-                "value": "no",
-                "text": {
-                  "default": "No",
-                  "fr": "Non merci."
-                }
-              }
-            ]
+          "maxLength": 300
+        },
+        {
+          "type": "comment",
+          "name": "improvements-required",
+          "visibleIf": "{nps-score} >= 7",
+          "title": {
+            "default": improvements,
+            "fr": "Que pouvons-nous faire pour rendre votre expérience plus satisfaisante?"
           },
-          {
-            "type": "text",
-            "name": "email",
-            "visibleIf": "{testimonial} = 'yes'",
-            "title": {
-              "default": "What is your email address?",
-              "fr": "Quelle est votre adresse e-mail?"
+          "maxLength": 300
+        },
+        {
+          "type": "checkbox",
+          "name": "promoter-features",
+          "visibleIf": "{nps-score} >= 9",
+          "title": {
+            "default": "Which of the following features do you value the most?",
+            "fr": "Laquelle des fonctionnalités suivantes appréciez-vous le plus ?"
+          },
+          "description": {
+            "default": "Please select no more than three features.",
+            "fr": "Veuillez ne pas sélectionner plus de trois fonctionnalités."
+          },
+          "isRequired": true,
+          "choices": checkboxes,
+          "showOtherItem": true,
+          "otherPlaceholder": {
+            "default": "Please specify...",
+            "fr": "Veuillez préciser..."
+          },
+          "otherText": {
+            "default": "Other features",
+            "fr": "Autres fonctionnalités"
+          },
+          "colCount": 2,
+          "maxSelectedChoices": 3
+        },
+        {
+          "type": "checkbox",
+          "name": "passive/defractor-features",
+          "visibleIf": "{nps-score} < 9",
+          "title": {
+            "default": "Which of the following features do you think need the most improvement?",
+            "fr": "Laquelle des fonctionnalités suivantes appréciez-vous le plus ?"
+          },
+          "description": {
+            "default": "Please select no more than three features.",
+            "fr": "Veuillez ne pas sélectionner plus de trois fonctionnalités."
+          },
+          "isRequired": true,
+          "choices": [
+            {
+              "value": "performance",
+              "text": "Performance"
             },
-            "validators": [
-              {
-                "type": "email"
+            {
+              "value": "stability",
+              "text": {
+                "default": "Stability",
+                "fr": "Stabilité"
               }
-            ],
-            "placeholder": {
-              "default": "Enter your email here",
-              "fr": "Veuillez saisir votre adresse e-mail ici"
+            },
+            {
+              "value": "ui",
+              "text": {
+                "default": "User interface",
+                "fr": "Interface utilisateur"
+              }
+            },
+            {
+              "value": "learning-materials",
+              "text": {
+                "default": "Documentations and Demos",
+                "fr": "Documentation et démos"
+              }
+            },
+            {
+              "value": "support",
+              "text": {
+                "default": "Quality support",
+                "fr": "Accompagnement de qualité"
+              }
             }
-          }
-        ]
-      }
-    ],
-    "showPrevButton": false,
-    "showQuestionNumbers": "off",
-    "completeText": {
-     "fr": "Envoyer"
+          ],
+          "showOtherItem": true,
+          "otherPlaceholder": {
+            "default": "Please specify...",
+            "fr": "Veuillez préciser..."
+          },
+          "otherText": {
+            "default": "Other features",
+            "fr": "Autres fonctionnalités"
+          },
+          "colCount": 2,
+          "maxSelectedChoices": 3
+        },
+        {
+          "type": "boolean",
+          "name": "rebuy",
+          "title": {
+            "default": "Would you buy our product again?",
+            "fr": "Achèteriez-vous à nouveau notre produit?"
+          },
+          "isRequired": true
+        },
+        {
+          "type": "text",
+          "name": "email",
+          "title": {
+            "default": "We respect your privacy. If you choose to provide your email, we'll use it solely for further correspondance relevant to this survey.",
+            "fr": "Achèteriez-vous à nouveau notre produit?"
+          },
+          "isRequred": false,
+          "validators": [
+            {
+              "type": "email",
+              "text": {
+                "default": "Please enter a valid email address.",
+                "fr": "Veuillez entrer une adresse e-mail valide."
+              }
+            }
+          ],
+          "inputType": "email",
+          "placeholder": {
+            "default": "yourname@example.com",
+            "fr": "votrenom@exemple.com"
+          },
+          
+        }
+      ]
     },
-    "widthMode": "static",
-    "width": "1000px"
-   };
+
+  ],
+  "showPrevButton": false,
+  "showQuestionNumbers": "off",
+  "completeText": {
+   "fr": "Envoyer"
+  },
+  "widthMode": "static",
+  "width": "1000px"
+ };
